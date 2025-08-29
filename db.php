2d376d33743464ae35a477ae40907798b839a78c
mahajar <hajar17maacha@gmail.com>
2025-08-29 09:44:35 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$db   = 'smarttaskdb';
+$user = 'root';
+$pass = ''; // mettez votre mot de passe MySQL ici
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
